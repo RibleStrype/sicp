6748de4ad02865f451033a53f0d0b28e5a71cd8b
Eriks Harcevs <eharcevs@evolutiongaming.com>
2020-08-11 17:53:16 +0300

    exercises up to 2.12

--- chapter_2.1.scm
+++ chapter_2.1.scm
@@ -187,6 +187,25 @@
           (lower-bound x)) 
        2))
 
-(define (spans-interval? x y)
-    (and (>= (upper-bound x) y)
-         (<= (lower-bound x) y)))
+(define (spans-interval? i x)
+    (and (>= (upper-bound i) x)
+         (<= (lower-bound i) x)))
+
+(define (make-center-width c w)
+    (make-interval (- c w) (+ c w)))
+
+(define (center i)
+  (/ (+ (lower-bound i) 
+        (upper-bound i)) 
+     2))
+
+(define (width i)
+  (/ (- (upper-bound i) 
+        (lower-bound i)) 
+     2))
+
+(define (make-center-percent c t)
+    (make-center-width c (* c (/ t 100))))
+
+(define (percent i)
+    (* (/ (width i) (center i)) 100))
