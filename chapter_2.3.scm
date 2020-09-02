7ce1f3888f058411e473ce31db9209903ca275b1
Eriks Harcevs <eharcevs@evolutiongaming.com>
2020-09-02 17:23:33 +0300

    exercise 2.70

--- chapter_2.3.scm
+++ chapter_2.3.scm
@@ -202,3 +202,135 @@
         ((= given-key (key (entry set-of-records))) (entry set-of-records))
         ((< given-key (key (entry set-of-records))) (lookup given-key (left-branch set-of-records)))
         (else (lookup given-key (right-branch set-of-records)))))
+
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+(define (symbol-leaf x) (cadr x))
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) 
+                (symbols right))
+        (+ (weight left) (weight right))))
+
+(define (left-branch tree) (car tree))
+(define (right-branch tree) (cadr tree))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (decode bits tree)
+  (define (choose-branch bit branch)
+    (cond ((= bit 0) (left-branch branch))
+          ((= bit 1) (right-branch branch))
+          (else (error "bad bit: CHOOSE-BRANCH" bit)))) 
+
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch
+               (choose-branch 
+                (car bits) 
+                current-branch)))
+          (if (leaf? next-branch)
+              (cons 
+               (symbol-leaf next-branch)
+               (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) 
+                        next-branch)))))
+
+  (decode-1 bits tree))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) 
+         (cons x set))
+        (else 
+         (cons (car set)
+               (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set 
+         (make-leaf (car pair)    ; symbol
+                    (cadr pair))  ; frequency
+         (make-leaf-set (cdr pairs))))))
+
+(define sample-tree
+  (make-code-tree 
+   (make-leaf 'A 4)
+   (make-code-tree
+    (make-leaf 'B 2)
+    (make-code-tree 
+     (make-leaf 'D 1)
+     (make-leaf 'C 1)))))
+
+(define sample-message 
+  '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+(define (encode message tree)
+  (define (encode-symbol symbol tree)
+    (cond ((null? tree) (error "no symbol in tree" symbol))
+          ((leaf? tree)
+            (if (eq? (symbol-leaf tree) symbol)
+                ()
+                (error "no symbol in tree" symbol)))
+          ((contains? symbol (symbols (left-branch tree)))
+            (cons 0 (encode-symbol symbol (left-branch tree))))
+          (else 
+            (cons 1 (encode-symbol symbol (right-branch tree))))))
+
+  (if (null? message)
+      ()
+      (append 
+       (encode-symbol (car message) tree)
+       (encode (cdr message) tree))))
+
+(define (contains? x list)
+  (cond ((null? list) #f)
+        ((eq? x (car list)) #t)
+        (else (contains? x (cdr list)))))
+
+(define (generate-huffman-tree pairs)
+  (define (successive-merge nodes)
+    (cond ((or (null? nodes)
+               (null? (cdr nodes))) (car nodes))
+          (else (successive-merge
+                  (adjoin-set (make-code-tree (car nodes) (cadr nodes))
+                              (cddr nodes))))))
+
+  (successive-merge 
+   (make-leaf-set pairs)))
+
+(define rock-tree
+  (generate-huffman-tree
+    (list
+      (list 'A 2)
+      (list 'BOOM 1)
+      (list 'GET 2)
+      (list 'JOB 2)
+      (list 'NA 16)
+      (list 'SHA 3)
+      (list 'YIP 9)
+      (list 'WAH 1))))
+(define rock-lyrics
+  '(Get a job 
+    Sha na na na na na na na na
+    Get a job
+    Sha na na na na na na na na
+    Wah yip yip yip yip
+    yip yip yip yip yip
+    Sha boom))
