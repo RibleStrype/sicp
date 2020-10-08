a76e0618d268abc929adcdcb6effa8f7086f7265
Eriks Harcevs <eharcevs@evolutiongaming.com>
2020-10-08 17:29:56 +0300

    exercise 2.87

--- chapter_2.5.scm
+++ chapter_2.5.scm
@@ -367,12 +367,118 @@
                                                                     imag-part
                                                                     table)))))))))))))
 
+(define variable? symbol?)
+(define (same-variable? x y)
+    (and (variable? x)
+         (variable? y)
+         (eq? x y)))
+
+(define (install-polynomial-package table)
+    (define (make-poly variable term-list)
+        (cons variable term-list))
+    (define (variable p) (car p))
+    (define (term-list p) (cdr p))
+
+    (define (add-poly p1 p2)
+        (if (same-variable? (variable p1)
+                            (variable p2))
+            (make-poly
+                (variable p1)
+                (add-terms (term-list p1)
+                           (term-list p2)))
+            (error "Polys not in same var: ADD-POLY" 
+                   (list p1 p2))))
+
+    (define (mul-poly p1 p2)
+        (if (same-variable? (variable p1)
+                            (variable p2))
+            (make-poly
+                (variable p1)
+                (mul-terms (term-list p1)
+                           (term-list p2)))
+            (error "Polys not in same var: MUL-POLY"
+                   (list p1 p2))))
+
+    (define (add-terms L1 L2)
+        (cond ((empty-termlist? L1) L2)
+              ((empty-termlist? L2) L1)
+              (else
+                (let* ((t1 (first-term L1))
+                       (t2 (first-term L2)))
+                    (cond ((> (order t1) (order t2)) 
+                            (adjoin-term
+                                t1
+                                (add-terms (rest-terms L1)
+                                           L2)))
+                          ((< (order t1) (order t2)) 
+                            (adjoin-term
+                                t2
+                                (add-terms L1
+                                           (rest-terms L2))))
+                          (else (adjoin-term
+                                    (make-term (order t1)
+                                               (add (coeff t1)
+                                                    (coeff t2)))
+                                    (add-terms (rest-terms L1)
+                                               (rest-terms L2)))))))))
+
+    (define (mul-terms L1 L2)
+        (if (empty-termlist? L1)
+            L1
+            (add-terms
+                (mul-term-by-all-terms
+                    (first-term L1)
+                    L2)
+                (mul-terms
+                    (rest-terms L1)
+                    L2))))
+
+    (define (mul-term t1 t2)
+        (make-term (add (order t1) (order t2))
+                   (mul (coeff t1) (coeff t2))))
+
+    (define (mul-term-by-all-terms t L)
+        (if (empty-termlist? L)
+            L
+            (adjoin-term
+                (mul-term t (first-term L))
+                (mul-term-by-all-terms t (rest-terms L)))))
+
+    (define (tag p) (attach-tag 'polynomial p))
+
+    (define (make-term order coeff)
+        (list order coeff))
+    (define order car)
+    (define coeff cadr)
+    (define empty-termlist? null?)
+    (define first-term car)
+    (define rest-terms cdr)
+    (define (adjoin-term t L)
+        (if (zero? (coeff t))
+            L
+            (cons t L)))
+
+    (put 'add '(polynomial polynomial)
+         (lambda (p1 p2)
+            (tag (add-poly p1 p2)))
+         (put 'mul '(polynomial polynomial)
+              (lambda (p1 p2)
+                (tag (mul-poly p1 p2)))
+              (put 'make 'polynomial
+                   (lambda (var terms)
+                     (tag (make-poly var terms)))
+                   (put 'zero? '(polynomial)
+                        empty-termlist?
+                        table)))))
+
 (define (make-rational n d)
   ((get 'make 'rational) n d))
 (define (make-complex-from-real-imag x y)
   ((get 'make-from-real-imag 'complex) x y))
 (define (make-complex-from-mag-ang r a)
   ((get 'make-from-mag-ang 'complex) r a))
+(define (make-polynomial var terms)
+    ((get 'make 'polynomial) var terms))
 
 (define (real-part z)
     (apply-generic 'real-part z))
@@ -386,4 +492,5 @@
 (define table 
         (install-scheme-number-package
             (install-rational-package 
-                (install-complex-package ()))))
+                (install-complex-package 
+                    (install-polynomial-package ())))))
